bb6788be82a5c4767b8de7fa273cb314279ea716
ingdanielb <danielb2889@misena.edu.co>
2023-03-08 07:30:35 -0500

    Carrito

--- hello_flutter/lib/products_list_page.dart
+++ hello_flutter/lib/products_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_flutter/product.dart';
 import 'package:hello_flutter/products.dart';
 
 import 'appbar_nav.dart';
@@ -11,6 +12,20 @@ class ProductsList extends StatefulWidget {
 }
 
 class _ProductsListState extends State<ProductsList> {
+  void aumentar(var i) {
+    setState(() {
+      i.quantity++;
+    });
+  }
+
+  void disminuir(var i) {
+    setState(() {
+      if (i.quantity > 0) {
+        i.quantity--;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,17 +39,62 @@ class _ProductsListState extends State<ProductsList> {
               const Divider(),
           itemBuilder: (BuildContext context, int index) {
             return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Container(
                   width: 90,
-                  height: 90,                  
+                  height: 90,
                   decoration: BoxDecoration(
-                    image: DecorationImage(image: NetworkImage(products[index].imageUrl!))
-                  ),
+                      image: DecorationImage(
+                          image: NetworkImage(products[index].imageUrl!))),
                 ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      products[index].name!,
+                      style: const TextStyle(
+                          fontSize: 17, fontWeight: FontWeight.bold),
+                    ),
+                    Text('\$ ${products[index].price}'),
+                    Container(
+                      height: 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                          border: Border.all(color: Colors.grey)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                disminuir(products[index]);
+                              },
+                              icon: Icon(Icons.remove)),
+                          Text('${products[index].quantity}'),
+                          IconButton(
+                              onPressed: () {
+                                aumentar(products[index]);
+                              },
+                              icon: Icon(Icons.add))
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                IconButton(
+                    onPressed: () {}, icon: Icon(Icons.add_shopping_cart))
               ],
-            );           
+            );
           },
-        ));
+        ),
+        
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          child: Icon(Icons.shopping_cart),
+          backgroundColor: Colors.red,
+
+          ),
+        );
   }
 }
