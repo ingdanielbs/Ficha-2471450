c9a18f4f6ee75691c35ac6cdcef44d6ab25c3a4b
ingdanielb <danielb2889@misena.edu.co>
2023-03-13 11:59:38 -0500

    Formularios

---
+++ formularios/lib/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Formularios')),
+      body: Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(      
+            decoration: const InputDecoration(label: Text('Nombre')),      
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+        ],
+      ),
+    ),
+    );
+  }
+}
--- hello_flutter/lib/cart_page.dart
+++ hello_flutter/lib/cart_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hello_flutter/product.dart';
 
+import 'appbar_nav.dart';
+
 class CartPage extends StatefulWidget {
   final List<Product> cart;
   const CartPage({super.key, required this.cart});
@@ -12,37 +14,65 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
+    double total =
+        widget.cart.fold(0, (sum, item) => sum + (item.price! * item.quantity));
     return Scaffold(
-        body: ListView.separated(
-      itemCount: widget.cart.length,
-      separatorBuilder: (BuildContext context, int index) => const Divider(),
-      itemBuilder: (BuildContext context, int index) {
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              width: 90,
-              height: 90,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(widget.cart[index].imageUrl!))),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  widget.cart[index].name!,
-                  style: const TextStyle(
-                      fontSize: 17, fontWeight: FontWeight.bold),
-                ),
-                Text('\$ ${widget.cart[index].price}'),
-              ],
-            ),
-            Text('${widget.cart[index].quantity}'),
-            IconButton(onPressed: () {}, icon: Icon(Icons.remove_shopping_cart))
-          ],
-        );
-      },
-    ));
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(70),
+        child: AppBarNav(title: 'Farmacia'),
+      ),
+      body: ListView.separated(
+        itemCount: widget.cart.length,
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        itemBuilder: (BuildContext context, int index) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 90,
+                height: 90,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(widget.cart[index].imageUrl!))),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    widget.cart[index].name!,
+                    style: const TextStyle(
+                        fontSize: 17, fontWeight: FontWeight.bold),
+                  ),
+                  Text('\$ ${widget.cart[index].price}'),
+                ],
+              ),
+              Text('${widget.cart[index].quantity}'),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      widget.cart.removeAt(index);
+                    });
+                  },
+                  icon: const Icon(Icons.remove_shopping_cart))
+            ],
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            setState(() {
+              if (widget.cart.isNotEmpty) {
+                widget.cart.clear();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Compra exitosa'),
+                    backgroundColor: Color.fromARGB(255, 85, 143, 86),
+                  ),
+                );
+              }
+            });
+          },
+          label: Text('Total \$$total')),
+    );
   }
 }
